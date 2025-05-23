8938af1ecabb02b7db40b6b06e98331edc5b8365
testing2122 <lolwothappened15+1@gmail.com>
2025-05-23 16:28:15 +0100

    Initial project structure: main.lua and README.md

---
+++ main.lua
@@ -0,0 +1,14 @@
+-- // main.lua - Entry point for the UI system
+
+-- // Xenon, this is like, the main gateway to our awesome UI! totes! <3
+
+local function init()
+  print("ui system initializing...")
+  -- // Later, we'll load the example from here
+  -- // local ideLayout = loadstring(readfile("examples/IDELayout.lua"))()
+  -- // if ideLayout and ideLayout.mount then
+  -- //   ideLayout:mount()
+  -- // end
+end
+
+init();
